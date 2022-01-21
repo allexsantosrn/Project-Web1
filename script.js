c20b5ecd448f59bb95bb2bd9738c9640285c972d
allexkid <allexsantosrn@gmail.com>
2022-01-21 11:28:34 -0300

    Update Combos
    Testando a alteração de combos.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const main = document.getElementById('main');
-
 const flexRadioDefault1 = document.getElementById('flexRadioDefault1');
 const flexRadioDefault2 = document.getElementById('flexRadioDefault2');
 
@@ -31,5 +29,52 @@ function tasksTitle() {
     }
 }
 
+function carryUsers() {
 
+    var comboUsers = document.getElementById("comboUsers");
+
+    var opt2 = document.createElement("option");
+    opt2.value = 1;
+    opt2.text = "Santo André";
+    comboUsers.add(opt2, comboUsers.options[1]);
+
+    var opt2 = document.createElement("option");
+    opt2.value = 2;
+    opt2.text = "Rio de Janeiro";
+    comboUsers.add(opt2, comboUsers.options[1]);
+} 
+
+/*
+ * Função AJAX base do tipo assíncrona.
+ * type é o tipo de objeto que você quer recuperar.
+ * value é o valor do parâmetro para filtrar os resultados dos tipos 2, 3 e 4.
+ * [Importante!] Você não pode, em nenhuma hipótese, alterar a função xhttpAssincrono.
+ */
+function xhttpAssincrono(callBackFunction, type, value) {
+    var xhttp = new XMLHttpRequest();;
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            // Chama a função em callback e passa a resposta da requisição
+            callBackFunction(this.responseText);
+        }
+    };
+    // Path para a requisição AJAX.
+    var url = "http://jsonplaceholder.typicode.com/";
+    switch (type) {
+        case 1:
+            url += "users"
+            break;
+        case 2:
+            url += "posts?userId=" + value;
+            break;
+        case 3:
+            url += "todos?userId=" + value;
+            break;
+        case 4:
+            url += "comments?postId=" + value;
+            break;
+    }
+    xhttp.open("GET", url, true);
+    xhttp.send();
+}
 
